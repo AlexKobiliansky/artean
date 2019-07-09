4e813fcd8e8e561ae211442e7c10389b61bb16b8
alex <alex_kobilianskiy@bigmir.net>
2019-07-09 22:07:32 +0300

    section video complete

--- app/js/common.js
+++ app/js/common.js
@@ -47,6 +47,117 @@ $(document).ready(function(){
      * end mobile-mnu customization
      */
 
+    /**
+     * VIDEO
+     */
+
+
+
+    //youtube script
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var vp;
+
+    var $playerID = 'videoPlayer-0';
+
+    onYouTubeIframeAPIReady = function () {
+
+        $("a[href='#video-popup']").on('click', function(){
+
+
+            var $videoID = $(this).data("video");
+
+
+            vp = new YT.Player($playerID, {
+                videoId: $videoID,
+                playerVars: {
+                    'autoplay': 1,
+                    'rel': 0,
+                    'showinfo': 0
+                },
+                events: {
+                    'onStateChange': onPlayerStateChange
+                }
+            });
+        });
+    };
+
+
+
+
+    onPlayerStateChange = function (event) {
+        if (event.data == YT.PlayerState.ENDED) {
+            $.magnificPopup.close();
+        }
+    };
+
+
+
+    $(function () {
+        $("a[href='#video-popup']").magnificPopup({
+            type: "inline",
+            fixedContentPos: !1,
+            fixedBgPos: !0,
+            overflowY: "auto",
+            closeBtnInside: !0,
+            preloader: !1,
+            midClick: !0,
+            removalDelay: 300,
+            mainClass: "my-mfp-zoom-in",
+            callbacks: {
+                close: function(){
+                    vp.stopVideo();
+                    vp.destroy();
+                }
+            }
+        })
+    });
+
+
+
+
+
+
+    /**
+     * end VIDEO
+     */
+
+
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
